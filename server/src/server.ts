ca3079fd4dd611d8a4eb8eeef721305b75e23d9f
Wanderson Castro <wandersonce1@hotmail.com>
2023-01-16 15:32:51 -0800

    Setting prisma and database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,17 @@
-console.log('test')
+import Fastify from 'fastify';
+import {PrismaClient} from '@prisma/client'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany()
+
+  return habits;
+})
+
+app.listen({
+  port:3333
+}).then(() => {
+  console.log('HTTP server listening on port 3333')
+})
