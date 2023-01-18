776f51417a9a27d263afe74b95c461ff6c98613f
Wanderson Castro <wandersonce1@hotmail.com>
2023-01-18 07:55:01 -0800

    SummaryTable Weekdays

---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,19 @@
+const weekDays = ['S', 'M', 'T', 'W', 'T','F','S'];
+
+export default function SummaryTable() {
+  return (
+    <div className='w-full flex'>
+      <div className='grid grid-rows-7 grid-flow-row gap 3'>
+
+        {weekDays.map((weekDay, i) => {
+          return(
+            <div key={`${weekDay}-${i}}`} className='text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center'>
+              {weekDay}
+            </div>
+          )
+        })}
+
+      </div>
+    </div>
+  )
+}
