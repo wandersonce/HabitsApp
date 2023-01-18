93f1cc37a67a901a2f73168d06e2ccdca6a6fac7
Wanderson Castro <wandersonce1@hotmail.com>
2023-01-18 07:42:53 -0800

    Creating header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,28 @@
-import Habit from "./components/Habit"
+import './styles/global.css';
+import logoImage from './assets/logo.svg'
+import {Plus} from 'phosphor-react'
+
 
 function App() {
   
   return (
-    <Habit completed={4}/>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+
+        <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
+          <img src={logoImage} alt="Habits" />
+
+          <button
+            type='button'
+            className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'
+          > 
+            <Plus size={20} className="text-violet-500"/>
+            New Habit
+          </button>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
