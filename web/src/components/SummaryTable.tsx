f6e1173537465cb75fe153ece3e8c427ffcce59a
Wanderson Castro <wandersonce1@hotmail.com>
2023-01-18 08:26:35 -0800

    Creating the squares for the rest of the year

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -5,7 +5,8 @@ const weekDays = ['S', 'M', 'T', 'W', 'T','F','S'];
 
 const summaryDates = generateDatesFromYearBeginning()
 
-console.log(summaryDates)
+const minimumSummaryDatesSize = 18 * 7 // 18 weeks to show at least
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
 
 export default function SummaryTable() {
   return (
@@ -26,6 +27,12 @@ export default function SummaryTable() {
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map(date => {
           return <HabitDay key={date.toString()}/>
+        })}
+
+        {amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill}).map((_ , i) => {
+          return (
+            <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
+          )
         })}
       </div>
     </div>
