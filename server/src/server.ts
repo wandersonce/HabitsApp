1262391dd7d3d41e0439e1f4fc6d0769a0e545bf
Wanderson Castro <wandersonce1@hotmail.com>
2023-01-19 07:37:49 -0800

    Changes on the back

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,14 @@
-import Fastify from 'fastify';
+import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { appRoutes } from './routes';
+import { appRoutes } from "./routes"
 
 const app = Fastify()
 
 app.register(cors)
 app.register(appRoutes)
 
-
 app.listen({
-  port:3333
+  port: 3333,
 }).then(() => {
-  console.log('HTTP server listening on port 3333')
+  console.log('HTTP Server running!')
 })
